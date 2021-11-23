42eb0e8ff10c74c7d11e598c1c7044b943a83c6d
Juliano Boese <juliano.boese@gmail.com>
2021-11-23 15:32:09 -0300

    Requisitos 7 e 8

--- script.js
+++ script.js
@@ -11,4 +11,25 @@ function addListItem() {
   });
 }
 
-addListItem()
+addListItem();
+
+function findGreyItem() {
+  const listItems = orderedList.children;
+  for (let i = 0; i < listItems.length; i += 1) {
+    if (listItems[i].style.backgroundColor === 'rgb(128, 128, 128)') {
+      listItems[i].style.backgroundColor = 'white';
+    }
+  }
+}
+
+function greyBackground() {
+  orderedList.addEventListener('click', (event) => {
+    const item = event.target;
+    if (item.id !== 'lista-tarefas') {
+      findGreyItem();
+      item.style.backgroundColor = 'rgb(128, 128, 128)';
+    }
+  });
+}
+
+greyBackground();

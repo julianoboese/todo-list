0f772f2635db3ee7dbae7dc33275277898bfc606
Juliano Boese <juliano.boese@gmail.com>
2021-11-23 22:53:39 -0300

    Estiliza, finaliza linting e organiza código

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-const addButton = document.getElementById('criar-tarefa');
-const taskInput = document.getElementById('texto-tarefa');
-const orderedList = document.getElementById('lista-tarefas');
-const eraseButton = document.getElementById('apaga-tudo');
-const completedButton = document.getElementById('remover-finalizados');
-const saveButton = document.getElementById('salvar-tarefas');
-const upButton = document.getElementById('mover-cima');
-const downButton = document.getElementById('mover-baixo');
-const removeButton = document.getElementById('remover-selecionado');
+const taskList = 'lista-tarefas';
+const orderedList = document.getElementById(taskList);
 
 window.onload = () => {
   orderedList.innerHTML = localStorage.getItem('Tasks');
 };
 
 function addListItem() {
+  const taskInput = document.getElementById('texto-tarefa');
+  const addButton = document.getElementById('criar-tarefa');
   addButton.addEventListener('click', () => {
     const newTask = document.createElement('li');
     newTask.innerHTML = taskInput.value;
@@ -35,7 +30,7 @@ function resetGreyItem() {
 function greyBackground() {
   orderedList.addEventListener('click', (event) => {
     const item = event.target;
-    if (item.id !== 'lista-tarefas') {
+    if (item.id !== taskList) {
       resetGreyItem();
       item.classList.add('selected');
     }
@@ -47,7 +42,7 @@ greyBackground();
 function setAsCompleted() {
   orderedList.addEventListener('dblclick', (event) => {
     const item = event.target;
-    if (item.id !== 'lista-tarefas') {
+    if (item.id !== taskList) {
       if (item.classList.contains('completed')) {
         item.classList.remove('completed');
       } else {
@@ -60,6 +55,7 @@ function setAsCompleted() {
 setAsCompleted();
 
 function eraseList() {
+  const eraseButton = document.getElementById('apaga-tudo');
   eraseButton.addEventListener('click', () => {
     while (orderedList.firstChild) {
       orderedList.removeChild(orderedList.firstChild);
@@ -70,6 +66,7 @@ function eraseList() {
 eraseList();
 
 function eraseCompleted() {
+  const completedButton = document.getElementById('remover-finalizados');
   completedButton.addEventListener('click', () => {
     for (let i = orderedList.children.length - 1; i >= 0; i -= 1) {
       if (orderedList.children[i].classList.contains('completed')) {
@@ -82,6 +79,7 @@ function eraseCompleted() {
 eraseCompleted();
 
 function storeListItems() {
+  const saveButton = document.getElementById('salvar-tarefas');
   saveButton.addEventListener('click', () => {
     localStorage.setItem('Tasks', orderedList.innerHTML);
   });
@@ -90,6 +88,7 @@ function storeListItems() {
 storeListItems();
 
 function moveUp() {
+  const upButton = document.getElementById('mover-cima');
   upButton.addEventListener('click', () => {
     if (document.querySelector('.selected') !== null) {
       const selectedItem = document.querySelector('.selected');
@@ -103,6 +102,7 @@ function moveUp() {
 moveUp();
 
 function moveDown() {
+  const downButton = document.getElementById('mover-baixo');
   downButton.addEventListener('click', () => {
     if (document.querySelector('.selected') !== null) {
       const selectedItem = document.querySelector('.selected');
@@ -116,6 +116,7 @@ function moveDown() {
 moveDown();
 
 function removeSelected() {
+  const removeButton = document.getElementById('remover-selecionado');
   removeButton.addEventListener('click', () => {
     if (document.querySelector('.selected') !== null) {
       document.querySelector('.selected').remove();
